ff62f13b5c334f7a53ff0b68c8d8ac8282500d3b
Nichols <jnicamzn@a483e7ad3e54.ant.amazon.com>
2020-02-11 14:02:08 -0800

    Adding argparse

--- check-instance-type-available.py
+++ check-instance-type-available.py
@@ -13,11 +13,20 @@ TODO: parameterize the command inputs.
 TODO: variables in the Filter of describe_instance_type_offerings
 TODO: search the response for the correspond instance type.
 """
-
-from boto3 import Session
 from botocore.exceptions import ClientError
+from boto3 import Session
+import argparse
+
 sess = Session(profile_name='default', region_name='us-east-1')
 ec2 = sess.client('ec2')
+
+parser = argparse.ArgumentParser(
+    description='Return true or false if an instance type is available in a specific availability zone')
+parser.add_argument('--az', '-a', help='Specific availability zone')
+parser.add_argument('--instancetype', '-i',
+                    help='Specific instance type, e.g. t2.micro')
+parser.add_argument('--profile', '-p', help='AWS Profile name')
+
 
 response = ec2.describe_instance_type_offerings(
     DryRun=False,
