30764975fbab3168c70c2bab3d449663b3dd84a8
Nichols <jnicamzn@a483e7ad3e54.ant.amazon.com>
2020-02-25 18:23:10 -0800

    Add error checking code for * region * availability zone Clean up the dict and list extractions Add exit codes: * 0 - instance type available * 1 - instance type not available * 2 - error with either region or availability zone parameter

--- check-instance-type-available.py
+++ check-instance-type-available.py
@@ -12,14 +12,24 @@ Goal: Returns True if instance type is available
 DONE: parameterize the command inputs.
 DONE: variables in the Filter of describe_instance_type_offerings
 DONE: search the response for the correspond instance type.
+DONE: validate input against a valid set of regions and availability zones
 TODO: check for environment variable, re default region if region is not provided
+TODO: Add debug option with logging (https://docs.python.org/3/howto/logging.html)
 """
 from boto3 import Session
 from botocore.exceptions import ClientError
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(prog='PROG',
-                                 description='Return true or false if an instance type is available in a specific availability zone')
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description='Return true or false if an instance type is available in a specific availability zone',
+                                 epilog='''\
+    This will return: 
+        0 if the instance type is found in the region and AZ 
+        1 if the instance type is not found in the region and AZ
+        2 if there is an error with either the region or AZ being incorrectly specified
+                                          ''')
 parser.add_argument('--az', default='us-east-1a',
                     help='Specific availability zone')
 parser.add_argument('--instancetype', '-i', default='t2.micro',
@@ -29,19 +39,49 @@ parser.add_argument('--region', '-r', default='us-east-1',
 parser.add_argument('--profile', '-p', help='AWS Profile name')
 
 args = parser.parse_args()
-az = args.az
-instancetype = args.instancetype
-region = args.region
+az = args.az.lower()
+instancetype = args.instancetype.lower()
+region = args.region.lower()
 profile = args.profile
 
 if not (region):
     region = 'us-east-1'
 
+# The region name is hard coded here to allow the program to retreive region names.
+# There is a second sess and ec2 call that uses the region passed on the command line
+sess = Session(profile_name=profile, region_name='us-east-1')
+ec2 = sess.client('ec2')
+
+# ERROR CHECKING INPUT
+# Retrieves all regions/endpoints and checks against input value region
+region_list = ec2.describe_regions()['Regions']
+region_names = []
+for i in region_list:
+    region_names.append(i['RegionName'])
+
+if not region in region_names:
+    print('Error: region ' + region + ' not a valid AWS region')
+    sys.exit(2)
+
 sess = Session(profile_name=profile, region_name=region)
 ec2 = sess.client('ec2')
 
-# It looks like the AZ Location is not filtering the response, so the payload has a list of all AZs where the instance is available.
-# This results in a double filter later in the script.
+# Retrieves availability zones for the region specified on the command line
+# Confirms the availability zone is in the specified region
+az_list = ec2.describe_availability_zones()['AvailabilityZones']
+az_names = []
+for i in az_list:
+    az_names.append(i['ZoneName'])
+
+if not az in az_names:
+    print('Error: availability zone ' + az +
+          ' is not available in AWS region ' + region)
+    print('If you are receiving this you may have left off the --region parameter.')
+    sys.exit(2)
+
+# The AZ Location is not filtering the response to just the specified AZ.
+# The payload has a list of all AZs where the instance is available.
+# This results in a set of logic later in the script.
 response = ec2.describe_instance_type_offerings(
     LocationType='availability-zone',
     Filters=[
@@ -58,17 +98,21 @@ response = ec2.describe_instance_type_offerings(
     ],
 )
 
-# Extract the DICT to a LIST
-instanceofferings = response['InstanceTypeOfferings']
+# Extract the DICT to a LIST and make the code more readable
+instance_offerings = response['InstanceTypeOfferings']
 
 # Populate a list to search. Because the filter in the describe_instance_type_offerings call
 # returns all AZs, a second logic check is required.
-instancelist = []
-for i in instanceofferings:
+instance_list = []
+for i in instance_offerings:
     if i['Location'] == az:
-        instancelist.append(i['InstanceType'])
+        instance_list.append(i['InstanceType'])
 
-if instancetype in instancelist:
-    print('Yes, ' + instancetype + ' is available in ' + az)
+if instancetype in instance_list:
+    print('Yes, ' + instancetype + ' is available in ' +
+          az + ' in the ' + region + ' region.')
+    sys.exit(0)
 else:
-    print('No, ' + instancetype + ' is NOT available in ' + az)
+    print('No, ' + instancetype + ' is NOT available in ' +
+          az + ' in the ' + region + ' region.')
+    sys.exit(1)
